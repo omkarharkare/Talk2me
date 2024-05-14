9e78c1304c03df1b6227b17de6ae25451d66e892
Omkar <omkar.harkare@gmail.com>
2024-05-14 15:58:49 -0400

    Initialize audio recording
    - The recording is command controlled.
    - Option to download the recording with separate event.
    - Need to work on UI section with more frameworks.

---
+++ frontend/app/pages/Landing.js
@@ -0,0 +1,28 @@
+'use client'
+import React, {useEffect, useState} from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+export default function Landing() {
+    /*
+    useEffect(() => {
+        console.log("Try me")
+    }, [])
+    */
+   
+    // const [initial, setinitial] = useState(console.log("Welcome"));
+    const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({audio: true})
+
+    /*
+    function handleClick () {
+        setinitial(console.log("fuck off"))
+    }
+    */
+    return (
+        <div>
+            <p>{status}</p>
+            <button onClick={startRecording}>Start Recording</button>
+            <button onClick={stopRecording}>Stop Recording</button>
+            <video src={mediaBlobUrl} controls autoPlay loop />
+        </div>
+    )
+}
